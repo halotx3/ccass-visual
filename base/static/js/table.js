b733c1011f1b4e75277bd75c905f59a0ba5fa37e
Irvin Hobba <skiphobba@gmail.com>
2020-07-05 18:05:09 -0400

    updated table.js with code to sort celebrity data.

--- base/static/js/table.js
+++ base/static/js/table.js
@@ -1,3 +1,4 @@
+//
 const athurl = "http://serene-beach-38826.herokuapp.com/api/v1/athletes/year/";
 const celurl = "http://serene-beach-38826.herokuapp.com/api/v1/celebrities/year/";
 
@@ -43,47 +44,36 @@ function updateTable() {
 
     if (dataset === 'Celebrity') {
         d3.json(celurl+yearset).then(function(cel_data) {
-        var table = document.getElementById('YearlyTable')
-        table.innerHTML = ''
-        for (var i = 0; i < 10; i++){
-            var row = `<tr>
-                            <td>${cel_data[i].name}</td>
-                            <td>${cel_data[i].category}</td>
-                            <td>${cel_data[i].pay}</td>
-                    </tr>`
-            table.innerHTML += row
+            comparePay = compareValues('pay', 'desc');    // new
+            var sorted_cel_data = cel_data.sort(comparePay);    // new
+            var table = document.getElementById('YearlyTable')
+            table.innerHTML = ''
+            for (var i = 0; i < 10; i++){
+                var row = `<tr>
+                                <td>${sorted_cel_data[i].name}</td>
+                                <td>${sorted_cel_data[i].category}</td>
+                                <td>${sorted_cel_data[i].pay}</td>
+                        </tr>`
+                table.innerHTML += row
         }
      });
     }
 };
 
-
-//Good code below here:
-// d3.json(athurl).then(function(ath_data) {
-//     var table = document.getElementById('YearlyTable')
-
-//     for (var i = 0; i < ath_data.length; i++){
-//         var row = `<tr>
-//                         <td>${ath_data[i].name}</td>
-//                         <td>${ath_data[i].sport}</td>
-//                         <td>${ath_data[i].earnings}</td>
-//                 </tr>`
-//         table.innerHTML += row
-//     }
-//  });
-
-
-
-
-// d3.json(celurl).then(function(cel_data) {
-//     var table = document.getElementById('YearlyTable')
-
-//     for (var i = 0; i < cel_data.length; i++){
-//         var row = `<tr>
-//                         <td>${cel_data[i].name}</td>
-//                         <td>${cel_data[i].category}</td>
-//                         <td>${cel_data[i].pay}</td>
-//                 </tr>`
-//         table.innerHTML += row
-//     }
-//  });
+function compareValues(key, order = 'asc') {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      // property doesn't exist on either object
+      return 0;
+    }
+    const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return ((order === 'desc') ? (comparison * -1) : comparison);
+  };
+}
