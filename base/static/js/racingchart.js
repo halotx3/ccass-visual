a8cace9bf83a030ebf81e61f61bf4a02d024d7f2
Christopher Brownlee <cdbreach@gmail.com>
2020-07-06 22:48:13 -0400

    Ground set for alternative graphs

--- base/static/js/racingchart.js
+++ base/static/js/racingchart.js
@@ -1,30 +1,114 @@
-chart = {
-    // replay;
+
+// var options = {
+//     chart: {
+//       type: 'line'
+//     },
+//     series: [{
+//       name: 'sales',
+//       data: [30,40,35,50,49,60,70,91,125]
+//     }],
+//     xaxis: {
+//       categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+//     }
+//   }
   
-    const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, height]);
+//   var chart = new ApexCharts(document.querySelector("#gInsert"), options);
   
-    const updateBars = bars(svg);
-    const updateAxis = axis(svg);
-    const updateLabels = labels(svg);
-    const updateTicker = ticker(svg);
-  
-    yield svg.node();
-  
-    for (const keyframe of keyframes) {
-      const transition = svg.transition()
-          .duration(duration)
-          .ease(d3.easeLinear);
-  
-      // Extract the top bar’s value.
-      x.domain([0, keyframe[1][0].value]);
-  
-      updateAxis(keyframe, transition);
-      updateBars(keyframe, transition);
-      updateLabels(keyframe, transition);
-      updateTicker(keyframe, transition);
-  
-      invalidation.then(() => svg.interrupt());
-      await transition.end();
+//   chart.render();
+
+var options = {
+    series: [
+    {
+      name: 'Bob',
+      data: [
+        {
+          x: 'Design',
+          y: [
+            new Date('2019-03-05').getTime(),
+            new Date('2019-03-08').getTime()
+          ]
+        },
+        {
+          x: 'Code',
+          y: [
+            new Date('2019-03-08').getTime(),
+            new Date('2019-03-11').getTime()
+          ]
+        },
+        {
+          x: 'Test',
+          y: [
+            new Date('2019-03-11').getTime(),
+            new Date('2019-03-16').getTime()
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Joe',
+      data: [
+        {
+          x: 'Design',
+          y: [
+            new Date('2019-03-02').getTime(),
+            new Date('2019-03-05').getTime()
+          ]
+        },
+        {
+          x: 'Code',
+          y: [
+            new Date('2019-03-06').getTime(),
+            new Date('2019-03-09').getTime()
+          ]
+        },
+        {
+          x: 'Test',
+          y: [
+            new Date('2019-03-10').getTime(),
+            new Date('2019-03-19').getTime()
+          ]
+        }
+      ]
     }
+  ],
+    chart: {
+    height: 350,
+    type: 'rangeBar'
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true
+    }
+  },
+//   dataLabels: {
+//     enabled: true,
+//     formatter: function(val) {
+//       var a = moment(val[0])
+//       var b = moment(val[1])
+//       var diff = b.diff(a, 'days')
+//       return diff + (diff > 1 ? ' days' : ' day')
+//     }
+//   },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'light',
+      type: 'vertical',
+      shadeIntensity: 0.25,
+      gradientToColors: undefined,
+      inverseColors: true,
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [50, 0, 100, 100]
+    }
+  },
+  xaxis: {
+    type: 'datetime'
+  },
+  legend: {
+    position: 'top'
   }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#gInsert"), options);
+  chart.render();
