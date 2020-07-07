6ec22890fd2523237b624e8d587e4d80290f4a53
Christopher Brownlee <cdbreach@gmail.com>
2020-07-07 14:43:53 -0400

    Updated to new table and inserted it. Remaining work on switch

--- base/static/js/racingchart.js
+++ base/static/js/racingchart.js
@@ -1,114 +1,117 @@
+//Grabbing URLs
+const athletesURL = 'http://serene-beach-38826.herokuapp.com/api/v1/athletes';
+// const celebURL;
 
-// var options = {
-//     chart: {
-//       type: 'line'
-//     },
-//     series: [{
-//       name: 'sales',
-//       data: [30,40,35,50,49,60,70,91,125]
-//     }],
-//     xaxis: {
-//       categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-//     }
-//   }
-  
-//   var chart = new ApexCharts(document.querySelector("#gInsert"), options);
-  
-//   chart.render();
 
-var options = {
-    series: [
-    {
-      name: 'Bob',
-      data: [
-        {
-          x: 'Design',
-          y: [
-            new Date('2019-03-05').getTime(),
-            new Date('2019-03-08').getTime()
-          ]
-        },
-        {
-          x: 'Code',
-          y: [
-            new Date('2019-03-08').getTime(),
-            new Date('2019-03-11').getTime()
-          ]
-        },
-        {
-          x: 'Test',
-          y: [
-            new Date('2019-03-11').getTime(),
-            new Date('2019-03-16').getTime()
-          ]
+//Temp Arrays to hold the years of each decade
+
+
+//Function to update tables based on decade
+
+d3.json(athletesURL).then(function(athlData){
+    // Ndata = athlData.filter(athlData.year <= 1990 && athlData.year < 2000)
+    let nameList = [];
+    // console.log(athlData.length)
+    let avgData = [];
+    //Loop to add individual names to name list
+    for(let i = 0; i < athlData.length; i++){
+        if(nameList.includes(athlData[i].name) === false){
+            nameList.push(athlData[i].name);
+            avgData.push({"name": athlData[i].name, "avg": athlData[i].earnings})
         }
-      ]
-    },
-    {
-      name: 'Joe',
-      data: [
-        {
-          x: 'Design',
-          y: [
-            new Date('2019-03-02').getTime(),
-            new Date('2019-03-05').getTime()
-          ]
-        },
-        {
-          x: 'Code',
-          y: [
-            new Date('2019-03-06').getTime(),
-            new Date('2019-03-09').getTime()
-          ]
-        },
-        {
-          x: 'Test',
-          y: [
-            new Date('2019-03-10').getTime(),
-            new Date('2019-03-19').getTime()
-          ]
+        else if(nameList.includes(athlData[i].name) === true){
+            for(let j = 0; j < avgData.length; j++){
+                if(athlData[i].name == avgData[j].name){
+                    let newAvg = (avgData[j].avg + athlData[i].earnings)/2
+                    avgData[j]["avg"] = newAvg
+                    console.log("average updated")
+                }
+            }
         }
-      ]
-    }
-  ],
-    chart: {
-    height: 350,
-    type: 'rangeBar'
-  },
-  plotOptions: {
-    bar: {
-      horizontal: true
-    }
-  },
-//   dataLabels: {
-//     enabled: true,
-//     formatter: function(val) {
-//       var a = moment(val[0])
-//       var b = moment(val[1])
-//       var diff = b.diff(a, 'days')
-//       return diff + (diff > 1 ? ' days' : ' day')
-//     }
-//   },
-  fill: {
-    type: 'gradient',
-    gradient: {
-      shade: 'light',
-      type: 'vertical',
-      shadeIntensity: 0.25,
-      gradientToColors: undefined,
-      inverseColors: true,
-      opacityFrom: 1,
-      opacityTo: 1,
-      stops: [50, 0, 100, 100]
-    }
-  },
-  xaxis: {
-    type: 'datetime'
-  },
-  legend: {
-    position: 'top'
-  }
-  };
+    };
+    //Sorting by highest average
+    sortAvg = compareValues('avg', 'desc');
+    let sortedAvg = avgData.sort(sortAvg)
 
-  var chart = new ApexCharts(document.querySelector("#gInsert"), options);
-  chart.render();
+    //Loop to save the Top 10
+    topNames = [];
+    topAvgs = [];
+    for(let i = 0; i < 10; i++){
+        topNames.push(sortedAvg[i].name)
+        topAvgs.push(Math.round(sortedAvg[i].avg, 1))
+    }
+    //posting chart
+    var options = {
+        series: [{
+        data: topAvgs
+      }],
+        chart: {
+        type: 'bar',
+        height: 450
+      },
+      annotations: {
+        xaxis: [{
+          x: 140,
+          borderColor: '#00E396',
+          label: {
+            borderColor: '#00E396',
+            style: {
+              color: '#fff',
+              background: '#00E396',
+            },
+            text: 'Yearly Average Career Earnings',
+          }
+        }],
+        yaxis: [{
+          y: topNames[2],
+          y2: topNames[6],
+        }]
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: true
+      },
+      xaxis: {
+        categories: topNames,
+      },
+      grid: {
+        xaxis: {
+          lines: {
+            show: true
+          }
+        }
+      },
+      yaxis: {
+        reversed: true,
+        axisTicks: {
+          show: true
+        }
+      }
+      };
+
+      var chart = new ApexCharts(document.querySelector("#gInsert"), options);
+      chart.render();
+})
+
+
+// function runningTable() {
+//     switch(decadeChoice) {
+//         case "1990s":
+//             d3.json(athletesURL).then(function(athlData){
+//                 Ndata = athlData.filter(athlData.year <= 1990 && athlData.year < 2000)
+//             })
+//         break;
+
+//         case "2000s":
+
+//         break;
+
+//         case "2010s":
+
+//         break;
+//     };
+// };
